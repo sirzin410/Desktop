fc790dac6c66c4b4a6c62eff907ae816f80550b9
sirzin410 <zingwevhu@gmail.com>
2026-03-09 13:58:20 +0200

    updated music finale

---
+++ musicmanager/music-list.js
@@ -0,0 +1,41 @@
+let allMusic = [{
+        name: "Snokonoko",
+        artist: "Benzo,Alshapo,Eexque",
+        img: "music-1",
+        src: "music-1",
+    },
+
+    {
+        name: "Baby Riddim",
+        artist: "Fave",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Free Mind",
+        artist: "Tems",
+        img: "music-3",
+        src: "music-3",
+    },
+
+    {
+        name: "Martin & Gina",
+        artist: "Polo G",
+        img: "music-4",
+        src: "music-4",
+    },
+    {
+        name: "Charm",
+        artist: "Rema",
+        img: "music-5",
+        src: "music-5",
+    },
+    {
+        name: "Uthando lwethu",
+        artist: "Ama Siblings,Dj Mngadi",
+        img: "music-6",
+        src: "music-6",
+    },
+
+
+]
